bf1ec0a38ab3768ff89b2a28c37299c5a49711d8
Karl Eklund <localpart@gmail.com>
2022-12-06 01:19:27 +0100

    day 1 part 2

--- day1.el
+++ day1.el
@@ -7,3 +7,15 @@
        (--map (-map #'string-to-number it))
        (-map #'-sum)
        (-max)))
+
+;; Part Two
+(with-temp-buffer
+  (insert-file-contents "input1.txt")
+  (->> (buffer-string)
+       (funcall (-flip #'split-string) "\n")
+       (-partition-after-pred #'string-empty-p)
+       (--map (-map #'string-to-number it))
+       (-map #'-sum)
+       (-sort #'>)
+       (-take 3)
+       (-sum)))
