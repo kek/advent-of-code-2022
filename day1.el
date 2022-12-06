1e0c4d6fa8b76cca4e4b1f63df0e2a2749eedd5f
Karl Eklund <localpart@gmail.com>
2022-12-06 01:19:14 +0100

    Refactor

--- day1.el
+++ day1.el
@@ -1,9 +1,9 @@
-(setq filename "input1.txt")
+;; Part One
 (with-temp-buffer
-  (insert-file-contents filename)
-  (let* ((food-item-strings (split-string (buffer-string) "\n"))
-         (bags-of-strings (-partition-after-pred #'string-empty-p food-item-strings))
-         (bags-of-numbers (mapcar (lambda (item) (mapcar #'string-to-number item)) bags-of-strings))
-         (bag-calories (mapcar #'-sum bags-of-numbers))
-         (max-bag-calories (-max bag-calories)))
-    max-bag-calories))
+  (insert-file-contents "input1.txt")
+  (->> (buffer-string)
+       (funcall (-flip #'split-string) "\n")
+       (-partition-after-pred #'string-empty-p)
+       (--map (-map #'string-to-number it))
+       (-map #'-sum)
+       (-max)))
