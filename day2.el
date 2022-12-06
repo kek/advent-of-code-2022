c91907145095ff2b2eb1189174c6a7208df8008f
Karl Eklund <localpart@gmail.com>
2022-12-06 20:09:36 +0100

    Day 2, part one

--- day2.el
+++ day2.el
@@ -8,7 +8,50 @@
 (princ "Day two\n")
 
 ;; Part One
+;; A,X Rock
+;; B,Y Paper
+;; C,Z Scissors
+(defun result (left right)
+  (cond ((equal left "A")
+         (cond
+          ((equal right "X") 'draw)
+          ((equal right "Y") 'right)
+          ((equal right "Z") 'left)))
+        ((equal left "B")
+         (cond
+          ((equal right "X") 'left)
+          ((equal right "Y") 'draw)
+          ((equal right "Z") 'right)))
+        ((equal left "C")
+         (cond
+          ((equal right "X") 'right)
+          ((equal right "Y") 'left)
+          ((equal right "Z") 'draw)))))
+
+(defun result-score (left right)
+  (let ((r (result left right)))
+    (cond ((eq r 'left) 0)
+          ((eq r 'right) 6)
+          (t 3))))
+
+(defun value (pick)
+  (cond
+   ((equal pick "X") 1)
+   ((equal pick "Y") 2)
+   ((equal pick "Z") 3)))
+
+(defun score (game)
+  (let* ((left (car game))
+         (right (cadr game)))
+    (+ (result-score left right)
+       (value right))))
+
+(score '("A" "Y"))
+
 (->> (read-input "input2.txt")
-     (-take 1)
-     (format "Something: %s\n")
-     (princ))
+     (-map #'string-split)
+     (--reject (equal it nil))
+     (-map #'score)
+     (-sum)
+     (princ)
+     )
