6ffeeb06ca90beee1d9030dede033e41172e9ab8
Karl Eklund <localpart@gmail.com>
2022-12-13 14:22:29 +0100

    day 2, part two

--- day2.el
+++ day2.el
@@ -46,12 +46,49 @@
     (+ (result-score left right)
        (value right))))
 
-(score '("A" "Y"))
 
 (->> (read-input "input2.txt")
      (-map #'string-split)
      (--reject (equal it nil))
      (-map #'score)
+     (-sum))
+
+;; Part two
+
+;; A,X Rock
+;; B,Y Paper
+;; C,Z Scissors
+;; X lose
+;; Y draw
+;; Z win
+(defun choice-for (other-choice result-needed)
+  (cond
+   ((equal result-needed "X") ; lose
+         (cond ((equal other-choice "A") "Z")
+               ((equal other-choice "B") "X")
+               ((equal other-choice "C") "Y")))
+
+   ((equal result-needed "Y") ; draw
+         (cond ((equal other-choice "A") "X")
+               ((equal other-choice "B") "Y")
+               ((equal other-choice "C") "Z")))
+
+   ((equal result-needed "Z") ; win
+    (cond ((equal other-choice "A") "Y")
+          ((equal other-choice "B") "Z")
+          ((equal other-choice "C") "X")))))
+
+(defun choose-moves (game)
+  (let* ((opponent-chooses (car game))
+         (result-needed (cadr game)))
+    (list
+     opponent-chooses
+     (choice-for opponent-chooses result-needed))))
+
+(->> (read-input "input2.txt")
+     (-map #'string-split)
+     (--reject (equal it nil))
+     (-map #'choose-moves)
+     (-map #'score)
      (-sum)
-     (princ)
-     )
+)
